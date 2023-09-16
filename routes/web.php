8bbc8fc83a883e7535064707e117a22dcefc646b
Cleissi Lima <cleissilima@gmail.com>
2023-09-16 19:50:17 -0300

    Criamos nosso primeiro controller Aprendemos sobre nomes de ações no Laravel Entendemos como Laravel lida com requisição e resposta

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SeriesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/ola', function () {
-    echo 'Olá, mundo!';
-});
+Route::get('/series', [SeriesController::class, 'index']); //primeiro elemento é a classe, o segundo elemento é o nome da função
